7ef2b24dc3ecda57d68b3b8da80bbb410c2964e0
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-12-13 22:30:06 -0500

    Adjusted spacing

--- encryption.php
+++ encryption.php
@@ -28,10 +28,10 @@ function encrypt( $plaintext )
     // Derive HMAC key
     $key_hmac = hash( 'sha256', md5(PASS_PHRASE) );
     // Initialization vector
-	$iv = random_bytes(16);
+    $iv = random_bytes(16);
 
-	$ciphertext = openssl_encrypt( $plaintext, $cipher, $key, 0, $iv );
-	$hash = hash_hmac( 'sha256', $ciphertext, $key_hmac );
+    $ciphertext = openssl_encrypt( $plaintext, $cipher, $key, 0, $iv );
+    $hash = hash_hmac( 'sha256', $ciphertext, $key_hmac );
 
     return base64_encode( $ciphertext . '::' . $hash . '::' . $iv );
 
@@ -54,17 +54,17 @@ function decrypt( $encrypted )
     // Derive encryption key
     $key = hash( 'sha256', PASS_PHRASE . md5(PASS_PHRASE) );
     // Derive HMAC key
-	$key_hmac = hash( 'sha256', md5(PASS_PHRASE) );
+    $key_hmac = hash( 'sha256', md5(PASS_PHRASE) );
 
-	list( $ciphertext, $hash, $iv ) = explode( '::', base64_decode($encrypted) );
-	$digest = hash_hmac( 'sha256', $ciphertext, $key_hmac );
+    list( $ciphertext, $hash, $iv ) = explode( '::', base64_decode($encrypted) );
+    $digest = hash_hmac( 'sha256', $ciphertext, $key_hmac );
 
     // HMAC authentication
     if ( hash_equals($hash, $digest) ) {
-		return openssl_decrypt( $ciphertext, $cipher, $key, 0, $iv );
-		}
-	else {
-		return 'Please verify authenticity of ciphertext.';
-	}
+        return openssl_decrypt( $ciphertext, $cipher, $key, 0, $iv );
+        }
+    else {
+        return 'Please verify authenticity of ciphertext.';
+    }
 
 }
