47097c28136cef6bec3ff6be61bf050962a34323
ray-ang <46805160+ray-ang@users.noreply.github.com>
2020-04-21 10:21:39 -0400

    Improved code for v0.9

--- encryption.php
+++ encryption.php
@@ -2,19 +2,19 @@
 /*
 Plugin Name: ClassicPress Encryption
 Plugin URI: https://github.com/ClassicPress-research/encryption-functions/
-Description: Encryption and decryption features for regulatory compliance.
+Description: Encryption features for GDPR and privacy laws compliance.
 Author: Raymund John Ang
 Author URI: https://open-nis.org/
-Text Domain: classicpress-encryption
+Text Domain: cp-encryption
 Version: 0.9
 */
 
-/** Show warning if CP_PASS_PHRASE is not defined */
+/** Show warning if CP_PASS_PHRASE is not defined. */
 add_filter( 'the_content', 'check_pass_phrase' );
 function check_pass_phrase() {
 
     if ( ! defined( 'CP_PASS_PHRASE' ) ) {
-        exit ('<strong>Warning: </strong>Constant CP_PASS_PHRASE is not defined in wp-config.php.');
+        exit( '<strong>Warning: </strong>Constant CP_PASS_PHRASE is not defined in wp-config.php.' );
     }
 
 }
@@ -40,6 +40,7 @@ function cp_encrypt( $plaintext ) {
 
 function cp_decrypt( $encrypted ) {
 
+    // Get encryption version from encoded string
     $version = explode( '::', $encrypted )[0];
 
     // Version-based decryption
@@ -62,11 +63,12 @@ function cp_decrypt( $encrypted ) {
 function cp_encrypt_v1( $plaintext )
 {
 
-    // Version
+    // Set encryption version
     $version = 'enc-v1';
 
-    // Cipher method to CBC with 256-bit key
+    // Set cipher method to CBC with 256-bit key
     $cipher = 'aes-256-cbc';
+
     // Salt for encryption key
     $salt_key = random_bytes(16);
     // Derive encryption key
@@ -81,6 +83,7 @@ function cp_encrypt_v1( $plaintext )
     $ciphertext = openssl_encrypt( $plaintext, $cipher, $key, 0, $iv );
     $hash = hash_hmac( 'sha256', $ciphertext, $key_hmac );
 
+    // Return encryption version and base64 encoding of ciphertext, hash, iv, salt_key, and salt_hmac
     return $version . '::' . base64_encode( $ciphertext ) . '::' . base64_encode( $hash ) . '::' . base64_encode( $iv ) . '::' . base64_encode( $salt_key ) . '::' . base64_encode( $salt_hmac );
 
 }
@@ -88,8 +91,8 @@ function cp_encrypt_v1( $plaintext )
 /**
  * Version 1 - Decrypt data using AES-CBC-HMAC
  *
- * @param string $encypted - base64_encoded version, ciphertext, hash,
- *                         - iv, salt_key, and salt_hmac
+ * @param string $encypted - encryption version and base64 encoding of
+ *                         - ciphertext, hash, iv, salt_key, and salt_hmac
  */
 
 function cp_decrypt_v1( $encrypted )
@@ -98,9 +101,10 @@ function cp_decrypt_v1( $encrypted )
     // Return empty if $encrypted is not set or empty.
     if ( ! isset( $encrypted ) || empty( $encrypted ) ) { return ''; }
 	
-    // Cipher method to CBC with 256-bit key
+    // Set cipher method to CBC with 256-bit key
     $cipher = 'aes-256-cbc';
 
+    // Extract encryption version and base64 encoded data from string
     list( $version, $ciphertext, $hash, $iv, $salt_key, $salt_hmac ) = explode( '::', $encrypted );
     $ciphertext = base64_decode( $ciphertext );
     $hash = base64_decode( $hash );
@@ -113,6 +117,7 @@ function cp_decrypt_v1( $encrypted )
     // Derive HMAC key
     $key_hmac = hash_pbkdf2( 'sha256', CP_PASS_PHRASE, $salt_hmac, 10000 );
     
+    // Variable to compare with $hash for HMAC authentication
     $digest = hash_hmac( 'sha256', $ciphertext, $key_hmac );
 
     // HMAC authentication
@@ -120,7 +125,7 @@ function cp_decrypt_v1( $encrypted )
         return openssl_decrypt( $ciphertext, $cipher, $key, 0, $iv );
         }
     else {
-        exit ('<strong>Warning: </strong>Please verify authenticity of ciphertext.');
+        return '<strong>Warning: </strong>Please verify authenticity of ciphertext.';
     }
 
 }
