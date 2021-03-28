d1ac21274e3ea8cbd9c1be1589dc6a8c27c3aaea
ray-ang <46805160+ray-ang@users.noreply.github.com>
2021-03-28 17:57:00 -0400

    Encrypt and decrypt buttons in admin post area

--- encryption.php
+++ encryption.php
@@ -6,127 +6,80 @@ Description: Encryption features for GDPR and privacy laws compliance.
 Author: Raymund John Ang
 Author URI: https://open-nis.org/
 Text Domain: cp-encryption
-Version: 0.9
-Credits: Ang, RJ (2019) BasicPHP source code (Version 0.9.2) [Source code]. https://github.com/ray-ang/basicphp
+Version: 0.9.1
+Credits: BasicPHP [Source code]. https://github.com/ray-ang/basicphp
+        Open-NIS Nursing ekardex [Source code]. https://github.com/ray-ang/open-nis-patient-care-summary
 */
 
-/** Show warning if CP_PASS_PHRASE is not defined. */
-add_filter( 'the_content', 'check_pass_phrase' );
-function check_pass_phrase() {
+if ( ! class_exists('Basic') ) require __DIR__ . '/Basic.php'; // BasicPHP class library
 
-    if ( ! defined( 'CP_PASS_PHRASE' ) ) {
-        exit( '<strong>Warning: </strong>Constant CP_PASS_PHRASE is not defined in wp-config.php.' );
+add_filter( 'admin_init', 'cp_check_pass_phrase' ); // Require passphrase alert
+
+function cp_check_pass_phrase() {
+
+    if ( ! defined( 'CP_PASS_PHRASE' ) && is_admin() && ! wp_doing_ajax() && ( stristr($_SERVER['REQUEST_URI'], 'post.php') || stristr($_SERVER['REQUEST_URI'], 'post-new.php') ) ) {
+        ?>
+        <script>alert('Warning: Constant CP_PASS_PHRASE is not defined in wp-config.php file.');</script>
+        <?php
     }
 
 }
 
 /**
- * Encrypt Data
- *
- * @param string $plaintext - Plaintext to be encrypted
- */
+  * Open-NIS Nursing ekardex post meta encrypt & decrypt buttons
+  */
+add_action( 'admin_init', 'cp_admin_encrypt_btn' ); // Encrypt and decrypt buttons
 
-function cp_encrypt( $plaintext ) {
+function cp_admin_encrypt_btn() {
 
-    // Default encryption function
-    return cp_encrypt_v1( $plaintext );
+    if( is_admin() && ! wp_doing_ajax() && isset($_POST['encrypt']) && $_POST['encrypt'] === 'Encrypt' && ! empty($_POST) ) {
 
-}
-
-/**
- * Decrypt Data
- *
- * @param string $encrypted - Data to be decrypted
- */
-
-function cp_decrypt( $encrypted ) {
-
-    // Get encryption version from encoded string
-    $version = explode( '::', $encrypted )[0];
-
-    // Version-based decryption
-    switch ( $version ) {
-        case 'enc-v1':
-            return cp_decrypt_v1( $encrypted );
-            break;
-        default:
-            return $encrypted;
-    }
-
-}
-
-/**
- * Version 1 - Encrypt data using AES-CBC-HMAC
- *
- * @param string $plaintext - Plaintext to be encrypted
- */
-
-function cp_encrypt_v1( $plaintext )
-{
-
-    // Set encryption version
-    $version = 'enc-v1';
-
-    // Set cipher method to CBC with 256-bit key
-    $cipher = 'aes-256-cbc';
-
-    // Salt for encryption key
-    $salt_key = random_bytes(16);
-    // Derive encryption key
-    $key = hash_pbkdf2( 'sha256', CP_PASS_PHRASE, $salt_key, 10000 );
-    // Salt for HMAC key
-    $salt_hmac = random_bytes(16);
-    // Derive HMAC key
-    $key_hmac = hash_pbkdf2( 'sha256', CP_PASS_PHRASE, $salt_hmac, 10000 );
-    // Initialization vector
-    $iv = random_bytes(16);
-
-    $ciphertext = openssl_encrypt( $plaintext, $cipher, $key, 0, $iv );
-    $hash = hash_hmac( 'sha256', $ciphertext, $key_hmac );
-
-    // Return encryption version and base64 encoding of ciphertext, hash, iv, salt_key, and salt_hmac
-    return $version . '::' . base64_encode( $ciphertext ) . '::' . base64_encode( $hash ) . '::' . base64_encode( $iv ) . '::' . base64_encode( $salt_key ) . '::' . base64_encode( $salt_hmac );
-
-}
-
-/**
- * Version 1 - Decrypt data using AES-CBC-HMAC
- *
- * @param string $encypted - encryption version and base64 encoding of
- *                         - ciphertext, hash, iv, salt_key, and salt_hmac
- */
-
-function cp_decrypt_v1( $encrypted )
-{
-
-    // Return empty if $encrypted is not set or empty.
-    if ( ! isset( $encrypted ) || empty( $encrypted ) ) { return ''; }
-	
-    // Set cipher method to CBC with 256-bit key
-    $cipher = 'aes-256-cbc';
-
-    // Extract encryption version and base64 encoded data from string
-    list( $version, $ciphertext, $hash, $iv, $salt_key, $salt_hmac ) = explode( '::', $encrypted );
-    $ciphertext = base64_decode( $ciphertext );
-    $hash = base64_decode( $hash );
-    $iv = base64_decode( $iv );
-    $salt_key = base64_decode( $salt_key );
-    $salt_hmac = base64_decode( $salt_hmac );
-
-    // Derive encryption key
-    $key = hash_pbkdf2( 'sha256', CP_PASS_PHRASE, $salt_key, 10000 );
-    // Derive HMAC key
-    $key_hmac = hash_pbkdf2( 'sha256', CP_PASS_PHRASE, $salt_hmac, 10000 );
-    
-    // Variable to compare with $hash for HMAC authentication
-    $digest = hash_hmac( 'sha256', $ciphertext, $key_hmac );
-
-    // HMAC authentication
-    if ( hash_equals( $hash, $digest ) ) {
-        return openssl_decrypt( $ciphertext, $cipher, $key, 0, $iv );
+        foreach ( $_POST['meta'] as $meta ) {
+            if ( ! stristr($meta['value'], 'encv') ) {
+                $index = array_search($meta, $_POST['meta']);
+                $_POST['meta'][$index]['value'] = Basic::encrypt($meta['value'], CP_PASS_PHRASE);
+            }
         }
-    else {
-        return '<strong>Warning: </strong>Please verify authenticity of ciphertext.';
+
+    }
+
+    if( is_admin() && ! wp_doing_ajax() && isset($_POST['decrypt']) && $_POST['decrypt'] === 'Decrypt' && ! empty($_POST) ) {
+
+        foreach ( $_POST['meta'] as $meta ) {
+            if ( stristr($meta['value'], 'encv') ) {
+                $index = array_search($meta, $_POST['meta']);
+                $_POST['meta'][$index]['value'] = Basic::decrypt($meta['value'], CP_PASS_PHRASE);
+            }
+        }
+
+    }
+
+}
+
+add_action( 'admin_footer', 'cp_admin_footer' ); // Admin - footer
+
+function cp_admin_footer() {
+
+    if ( is_admin() && ! wp_doing_ajax() && ( stristr($_SERVER['REQUEST_URI'], 'post.php') || stristr($_SERVER['REQUEST_URI'], 'post-new.php') ) ) {
+        ?>
+        <script>
+            // Render encrypt and decrypt buttons
+            const encryptBtn = document.createElement('input'); // Encrypt button
+            encryptBtn.classList.add('button', 'button-info', 'button-large');
+            encryptBtn.type = 'submit';
+            encryptBtn.name = 'encrypt';
+            encryptBtn.value = 'Encrypt';
+            encryptBtn.style.marginRight = '3px';
+            document.querySelector('#publishing-action').appendChild(encryptBtn);
+
+            const decryptBtn = document.createElement('input'); // Decrypt button
+            decryptBtn.classList.add('button', 'button-info', 'button-large');
+            decryptBtn.type = 'submit';
+            decryptBtn.name = 'decrypt';
+            decryptBtn.value = 'Decrypt';
+            document.querySelector('#publishing-action').appendChild(decryptBtn);
+        </script>
+        <?php
     }
 
 }
