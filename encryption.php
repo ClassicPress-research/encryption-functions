989cc9cbbe25bb7d45cd633745cdb6af597a6bd8
ray-ang <46805160+ray-ang@users.noreply.github.com>
2021-04-02 21:41:18 -0400

    Added Advanced Custom Fields compatibility

--- encryption.php
+++ encryption.php
@@ -49,10 +49,18 @@ function cp_admin_encrypt_btn() {
 
     if( is_admin() && ! wp_doing_ajax() && isset($_POST['encrypt']) && $_POST['encrypt'] === 'Encrypt' && ! empty($_POST) ) {
 
+        // Default WordPress custom fields
         foreach ( $_POST['meta'] as $meta ) {
-            if ( ! stristr($meta['value'], 'encv') ) {
+            if ( substr( ltrim($meta['value']), 0, 4 ) !== 'encv' ) {
                 $index = array_search($meta, $_POST['meta']);
                 $_POST['meta'][$index]['value'] = Basic::encrypt($meta['value'], CP_PASS_PHRASE);
+            }
+        }
+
+        // Advanced Custom Fields compatibility
+        foreach ( $_POST['acf'] as $key => $value ) {
+            if ( substr( ltrim($value), 0, 4 ) !== 'encv' ) {
+                $_POST['acf'][$key] = Basic::encrypt($value, CP_PASS_PHRASE);
             }
         }
 
@@ -60,10 +68,18 @@ function cp_admin_encrypt_btn() {
 
     if( is_admin() && ! wp_doing_ajax() && isset($_POST['decrypt']) && $_POST['decrypt'] === 'Decrypt' && ! empty($_POST) ) {
 
+        // Default WordPress custom fields
         foreach ( $_POST['meta'] as $meta ) {
-            if ( stristr($meta['value'], 'encv') ) {
+            if ( substr( ltrim($meta['value']), 0, 4 ) === 'encv' ) {
                 $index = array_search($meta, $_POST['meta']);
                 $_POST['meta'][$index]['value'] = Basic::decrypt($meta['value'], CP_PASS_PHRASE);
+            }
+        }
+
+        // Advanced Custom Fields compatibility
+        foreach ( $_POST['acf'] as $key => $value ) {
+            if ( substr( ltrim($value), 0, 4 ) === 'encv' ) {
+                $_POST['acf'][$key] = Basic::decrypt($value, CP_PASS_PHRASE);
             }
         }
 
